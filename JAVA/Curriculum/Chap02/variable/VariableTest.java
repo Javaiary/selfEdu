a80938359ab5f835cca665a0d7178fc8ac54a098
Javaiary <110982271+Javaiary@users.noreply.github.com>
2022-10-10 14:38:37 +0900

    variable

---
+++ JAVA/Curriculum/Chap02/variable/VariableTest.java
@@ -0,0 +1,17 @@
+package kr.co.ezneac.variable;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age,  count; 
+		age =10;
+		int level = 10000;
+		
+		System.out.println(age);
+		System.out.println(level);
+		
+		System.out.println(age + level);
+	}
+
+}
