86468dbe425f8ed2220c1360d22fe79cad16351c
Javaiary <110982271+Javaiary@users.noreply.github.com>
2022-10-10 14:39:09 +0900

    variable 02 (Double)

---
+++ JAVA/Curriculum/Chap02/variable02/DoubleTest.java
@@ -0,0 +1,10 @@
+package kr.co.ezneac.variable02;
+
+public class DoubleTest {
+	public static void main(String[] args) {
+		double dnum =3.14;
+		float fnum = 3.14F ; // Rvalue를 float값으로 지정
+		System.out.println(dnum);
+		System.out.println(fnum);
+	}
+}
---
+++ JAVA/Curriculum/Chap02/variable02/VariableTest.java
@@ -0,0 +1,12 @@
+package kr.co.ezneac.variable02;
+
+public class VariableTest {
+	public static void main(String[] args) {
+		byte b1 = 127;
+		System.out.println(b1);
+		
+		// int num = 12345678900; error
+		//long num = 12345678900; error
+		long num = 12345678900l;  // 정수 X, long형 데이터임을 표시
+				}
+}
