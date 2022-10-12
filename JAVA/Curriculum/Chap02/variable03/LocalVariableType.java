9aab9a0bb840643a937cbf0dba7a84fce68bc044
Javaiary <110982271+Javaiary@users.noreply.github.com>
2022-10-12 22:18:42 +0900

    Chap02 variable03

---
+++ JAVA/Curriculum/Chap02/variable03/LocalVariableType.java
@@ -0,0 +1,20 @@
+package kr.co.ezneac.variable03;
+
+public class LocalVariableType {
+
+	public static void main(String[] args) {
+		var i = 10 ;
+		var j = 10.0;
+		var str = "hello";
+		
+		System.out.println(j);
+		System.out.println(i);
+		System.out.println(str);
+		
+		//str= 3;  error
+		
+		
+				
+	}
+
+}
