25c07d8cce32fc389ea0e61583d67d08f23953a5
Nari Choi <110982271+Javaiary@users.noreply.github.com>
2022-10-17 09:25:55 +0900

    Chap03 constant

---
+++ JAVA/Curriculum/Chap 03/constant/TypeChange.java
@@ -0,0 +1,19 @@
+package rk.co.ezenac.constant;
+
+public class TypeChange {
+	public static void main(String[] args) {
+		// int형에서 최대로 표현할 수 있는 값
+		int num1 = 2147483647;
+		//int num2 = 2147483648; //int 범위 최대값을 벗어남.
+		//long num3 = 214748648; 
+ 		long num4 = 214748648L;
+ 		
+ 		//float num5 = 1.0;	//error 
+ 		float num6 = 1.0f;	
+ 		
+ 		double num7 = 30;	// int => double 자동 형변환
+ 		
+ 		
+ 		
+	}
+}
