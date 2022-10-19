f89a51bc51348ceea6a3bbabaf3a9f146d17888e
Nari Choi <110982271+Javaiary@users.noreply.github.com>
2022-10-19 22:28:54 +0900

    Chap 03 operator

---
+++ JAVA/Curriculum/Chap 03/operator/AssignmentTest.java
@@ -0,0 +1,17 @@
+package rk.co.ezenac.operator;
+
+public class AssignmentTest {
+
+	public static void main(String[] args) {
+		short num = 10;
+		num += 77L;
+		
+		System.out.println(num);
+
+		num = (short)(num + 77L) ;
+		System.out.println(num);
+		
+		
+	}
+
+}
