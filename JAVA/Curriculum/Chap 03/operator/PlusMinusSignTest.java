06030ef4114a425a41223719b1ea4a7b4f1498e1
Nari Choi <110982271+Javaiary@users.noreply.github.com>
2022-10-19 22:29:28 +0900

    Chap 03 operator

---
+++ JAVA/Curriculum/Chap 03/operator/PlusMinusSignTest.java
@@ -0,0 +1,13 @@
+package rk.co.ezenac.operator;
+
+public class PlusMinusSignTest {
+
+	public static void main(String[] args) {
+		short num1 = 5;
+		System.out.println(+num1);
+		System.out.println(-num1);	// 부호반전된 값을 출력. num1 값이 변한 것은 아님
+		
+
+	}
+
+}
