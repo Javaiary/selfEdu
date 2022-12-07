4fd53743a73880dbc17a9f4121513c79903bb090
Nari Choi <110982271+Javaiary@users.noreply.github.com>
2022-12-07 20:44:00 +0900

    Add files via upload

---
+++ JAVA/Curriculum/Chap05/variable/variable/BinaryTest.java
@@ -0,0 +1,16 @@
+package kr.co.ezenac.variable;
+
+public class BinaryTest {
+	public static void main(String[] args) {
+		int num = 10; 
+		int bNum = 0B1010; // 2진수값 0B____
+		int oNum = 012; // 8진수값 0____
+		int xNum = 0xA; // 16진수값 할당 0X___
+		
+		System.out.println(num);
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(xNum);
+		
+	}
+}
