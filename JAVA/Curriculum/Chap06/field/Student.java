7696b0142bf8dfe7dd85d92a0aef4653548d2e0f
Nari Choi <110982271+Javaiary@users.noreply.github.com>
2022-12-07 20:47:41 +0900

    Chap 06 field

---
+++ JAVA/Curriculum/Chap06/field/Student.java
@@ -0,0 +1,13 @@
+package kr.co.ezenac.field;
+
+public class Student {
+	//멤버 변수(필드)
+	int studentNumber;
+	String studentName;
+	int majorCode;
+	String majorName;
+	int grade;
+	
+
+	
+}
---
+++ JAVA/Curriculum/Chap06/field/User.java
@@ -0,0 +1,9 @@
+package kr.co.ezenac.field;
+
+public class User {
+	public String userId;
+	String userPassWord;
+	String userName;
+	String userAddress;
+	int phoneNumber;
+}
