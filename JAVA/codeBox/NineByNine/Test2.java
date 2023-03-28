d0a0f7f63334f9d16bf13bf56376c324a9b13dc7
Nari Choi <110982271+Javaiary@users.noreply.github.com>
2023-03-29 00:19:42 +0900

    Update Test2.java

--- JAVA/codeBox/NineByNine/Test2.java
+++ JAVA/codeBox/NineByNine/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 	
 
 	public void m1(int num) {
-		int arr[][] = new int[9][9];	//배열의 개수가 맞을까요?
+		int arr[][] = new int[8][9];	//배열의 개수가 맞을까요?
 		Test3 res = new Test3();
 		
 		for(int i=0; i < 8 ;i++) {	//2단부터 출력이 필요한데 맞을까요?
@@ -13,8 +13,8 @@ public class Test2 {
 				System.out.println(res.m2(num, j+1, arr[i][j])); //잘못된 부분입니다.
 				
 			}
+			System.out.println(Arrays.toString(arr[i]));
 			num++;
 		}
-		
 	}
 }
